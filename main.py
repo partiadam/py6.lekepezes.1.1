16b794005c2db7f1474eee775aed34648b84fb76
Parti Ádám <71691758+partiadam@users.noreply.github.com>
2022-02-14 13:41:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+1.1 Feladat
+Készíts egy programot, listaleképzéssel (Comprehensive list), amely a felhasználó által megadott szót, csupa nagybetűs formában írja ki a képernyőre!
+
+A megoldáshoz használd a sztringek upper() metódusát! 
+'''
+
+beker = input('Adj meg egy szót: ')
+eredmeny = [print(beker.upper())]
